2b31eddb31f5925af9b7a9969d3ab01c85dcde9d
Armand Agopian <armandagopian457@gmail.com>
2019-01-31 13:34:54 -0800

    LabExercise0

---
+++ src/Labs/LabExercise0/LabExercise0.cpp
@@ -0,0 +1,19 @@
+// Programmer: Sibbons Shrestha
+// Programmer's ID: 1595562
+// Programmer: Armand Agopian
+// Programmer's ID: 1612964
+
+#include <iostream>
+#include"LabExercise0.h"
+
+using namespace std;
+
+int main() {
+    cout << "Programmer: Sibbons Shrestha" << endl;
+    cout << "Programmer's ID: 1595562" << endl;
+    cout << "Programmer: Armand Agopian" << endl;
+    cout << "Programmer's ID: 1612964" << endl;
+    cout << "File: " << __FILE__ << endl;
+    welcome();
+    return 0;
+}
---
+++ src/Labs/LabExercise0/LabExercise0.h
@@ -0,0 +1,32 @@
+// Programmer: Sibbons Shrestha
+// Programmer's ID: 1595562
+// Programmer: Armand Agopian
+// Programmer's ID: 1612964
+
+#include<string>
+#include<iostream>
+
+using namespace std;
+
+void welcome() {
+    string firstName;
+    string lastName;
+
+    cout << "Please enter your first name :" << endl;
+    getline(cin, firstName);
+    cout << "Please enter your last name :" << endl;
+    getline(cin, lastName);
+
+    cout << endl << "COMSC 210 : Lab Excercise Zero" << endl;
+    cout << "=====================================" << endl;
+    cout << firstName << " " << lastName << " , Welcome to COMSC210-2704" << endl;
+    cout << firstName << endl;
+    cout << lastName << endl;
+    cout << "Enjot the hybrid format class!!" << endl;
+    cout << lastName << " " << firstName << endl;
+    cout << "This class will meet once a week at 11:10 in L-149 on Thursday" << endl;
+    cout << "Program ended with exit code : 0" << endl;
+    return;
+}
+
+#endif
