9634a6ad19475e3d91cf6d4ec2e692945f6d52d1
Armand Agopian <armandagopian457@gmail.com>
2019-02-03 14:34:40 -0800

    LabExercise0-Final

--- src/Labs/LabExercise0/LabExercise0.h
+++ src/Labs/LabExercise0/LabExercise0.h
@@ -3,6 +3,9 @@
 // Programmer: Armand Agopian
 // Programmer's ID: 1612964
 
+#ifndef COMPSCISPRING2019_LABEXERCISE0_H
+#define COMPSCISPRING2019_LABEXERCISE0_H
+
 #include<string>
 #include<iostream>
 
@@ -12,9 +15,9 @@ void welcome() {
     string firstName;
     string lastName;
 
-    cout << "Please enter your first name :" << endl;
+    cout << "Please enter your first name:" << endl;
     getline(cin, firstName);
-    cout << "Please enter your last name :" << endl;
+    cout << "Please enter your last name:" << endl;
     getline(cin, lastName);
 
     cout << endl << "COMSC 210 : Lab Excercise Zero" << endl;
@@ -26,7 +29,6 @@ void welcome() {
     cout << lastName << " " << firstName << endl;
     cout << "This class will meet once a week at 11:10 in L-149 on Thursday" << endl;
     cout << "Program ended with exit code : 0" << endl;
-    return;
 }
 
 #endif
