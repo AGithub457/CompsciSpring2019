14e680f21cf1de8b0a12ecb00bd3759471845208
Armand Agopian <armandagopian457@gmail.com>
2019-02-03 14:35:07 -0800

    Exercise1

---
+++ src/Exercises/Exercise1-CodeAlignment.cpp
@@ -0,0 +1,18 @@
+// Programmer: Armand Agopian
+// Programmer's ID: 1612964
+
+#include <string>
+
+using namespace std;
+
+int one(int, string);
+
+int main() {
+    int testing = one(1, "test");
+
+    return 0;
+}
+
+int one(int val, string name) {
+    return 1;
+}
