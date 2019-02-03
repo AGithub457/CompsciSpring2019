34a33862ea2d9636228fbc00107259e37210f26a
Armand Agopian <armandagopian457@gmail.com>
2019-02-03 14:37:13 -0800

    Exercise3

---
+++ src/Exercises/Exercise3-Console.cpp
@@ -0,0 +1,23 @@
+// Programmer: Armand Agopian
+// Programmer's ID: 1612964
+
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string input;
+
+    cout << "1 + 1 = ";
+    getline(cin, input);
+
+    if(input == "2") {
+        cout << "Correct!";
+    } else {
+        cout << "Incorrect :(";
+    }
+
+    return 0;
+}
